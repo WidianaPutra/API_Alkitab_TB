c10132e8a9e74e63ed381f15fc90c8df478eb1a8
WidianaPutra <olenggamer@gmail.com>
2026-05-28 21:53:37 +0800

    when lese

--- dist/lib/prisma.js
+++ dist/lib/prisma.js
@@ -1,10 +1,14 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.prisma = void 0;
 const adapter_better_sqlite3_1 = require("@prisma/adapter-better-sqlite3");
 const client_1 = require("@prisma/client");
+const node_path_1 = __importDefault(require("node:path"));
 const adapter = new adapter_better_sqlite3_1.PrismaBetterSqlite3({
-    url: "file:./dev.sqlite",
+    url: `file:${node_path_1.default.resolve(__dirname, "./dev.sqlite")}`,
 });
 exports.prisma = new client_1.PrismaClient({
     adapter,
