587a104b8a2e1b962c77986cf9f2308433c0fbb7
WidianaPutra <olenggamer@gmail.com>
2026-05-28 21:31:51 +0800

    push

--- dist/index.js
+++ dist/index.js
@@ -16,10 +16,11 @@ const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use("/verse", verse_route_1.default);
 app.use("/book", book_route_1.default);
-// if (process.env.NODE_ENV !== "production") {
-//   const SERVER_PORT = process.env.SERVER_PORT || 3000;
-//   app.listen(SERVER_PORT, () =>
-//     console.log(`server up and running on port ${SERVER_PORT}`),
-//   );
-// }
+app.get("/", (req, res) => {
+    return res.send("<div style='height: 100vh; width: 100%; display: flex; align-items: center; justify-content: center; background-color: black;'><h1 style='font-size: 40px; color: white;'>Alktab API TB</h1></div>");
+});
+if (process.env.NODE_ENV !== "production") {
+    const SERVER_PORT = process.env.SERVER_PORT || 3000;
+    app.listen(SERVER_PORT, () => console.log(`server up and running on port ${SERVER_PORT}`));
+}
 app.listen(SERVER_PORT, () => console.log(`server up and running on port ${SERVER_PORT}`));
