313852d4baf1663722a594c9508326f8d1d9bc20
WidianaPutra <olenggamer@gmail.com>
2026-05-28 21:46:06 +0800

    apakah ini last fixed dari prisma?

--- prisma.config.ts
+++ prisma.config.ts
@@ -12,6 +12,6 @@ export default defineConfig({
   datasource: {
     // url: process.env["DATABASE_URL"],
     // url: "file:./dev.sqlite",
-    url: `file:${path.resolve(__dirname, "../prisma/dev.sqlite")}`,
+    url: `file:${path.resolve(__dirname, "../dev.sqlite")}`,
   },
 });
