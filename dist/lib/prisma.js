2123f020dc377becf763f5e8f9750668afcdbb69
WidianaPutra <olenggamer@gmail.com>
2026-05-28 21:57:36 +0800

    apakah ini last?

--- dist/lib/prisma.js
+++ dist/lib/prisma.js
@@ -6,9 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.prisma = void 0;
 const adapter_better_sqlite3_1 = require("@prisma/adapter-better-sqlite3");
 const client_1 = require("@prisma/client");
-const node_path_1 = __importDefault(require("node:path"));
+const path_1 = __importDefault(require("path"));
 const adapter = new adapter_better_sqlite3_1.PrismaBetterSqlite3({
-    url: `file:${node_path_1.default.resolve(__dirname, "./dev.sqlite")}`,
+    url: `file:${path_1.default.resolve(process.cwd(), "dev.sqlite")}`,
 });
 exports.prisma = new client_1.PrismaClient({
     adapter,
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,9 @@
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "@prisma/client";
-import path from "node:path";
+import path from "path";
 
 const adapter = new PrismaBetterSqlite3({
-  url: `file:${path.resolve(__dirname, "./dev.sqlite")}`,
+  url: `file:${path.resolve(process.cwd(), "dev.sqlite")}`,
 });
 
 export const prisma = new PrismaClient({
