b14a87116940204ff24c41719880978b8ff157c2
WidianaPutra <olenggamer@gmail.com>
2026-05-28 21:53:19 +0800

    when lese

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,9 @@
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "@prisma/client";
+import path from "node:path";
 
 const adapter = new PrismaBetterSqlite3({
-  url: "file:./dev.sqlite",
+  url: `file:${path.resolve(__dirname, "./dev.sqlite")}`,
 });
 
 export const prisma = new PrismaClient({
